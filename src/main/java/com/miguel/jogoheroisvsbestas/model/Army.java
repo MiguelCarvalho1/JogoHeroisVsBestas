ae045ba0031d4fd6e448bc81463b1149115b0fc3
migue <miguelangelosilvacarvalho96@gmail.com>
2025-04-10 09:58:30 +0100

    Add Army logic functionality

--- src/main/java/com/miguel/jogoheroisvsbestas/model/Army.java
+++ src/main/java/com/miguel/jogoheroisvsbestas/model/Army.java
@@ -1,4 +1,30 @@
 package com.miguel.jogoheroisvsbestas.model;
 
+import java.util.*;
+
 public class Army {
+    private List<Character> characters;
+    private String name;
+
+    public Army( String name) {
+        this.characters = new ArrayList<>();
+        this.name = name;
+    }
+    public void addCharacter(Character character) {
+        characters.add(character);
+    }
+    public void removeDeadCharacters(){
+        characters.removeIf(c -> !c.isAlive());
+    }
+
+    public boolean hasAliveCharacters(){
+        return  characters.stream().anyMatch(Character::isAlive);
+    }
+    public List<Character> getCharacters() {
+        return characters;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
