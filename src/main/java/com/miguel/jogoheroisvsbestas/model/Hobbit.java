ee1668e90c4dcf4a54b3c980860790c062ecaee0
migue <miguelangelosilvacarvalho96@gmail.com>
2025-04-10 09:51:45 +0100

    Add implement Hobbit attack and Orc defense functionality

--- src/main/java/com/miguel/jogoheroisvsbestas/model/Hobbit.java
+++ src/main/java/com/miguel/jogoheroisvsbestas/model/Hobbit.java
@@ -4,4 +4,13 @@ public class Hobbit extends Hero{
     public Hobbit(String name, int health, int armor) {
         super(name, health, armor);
     }
+
+    @Override
+    public int calculateAttack(Character opponent) {
+        int baseAttack = super.calculateAttack(opponent);
+        if(opponent instanceof Troll){
+            return Math.max(0, baseAttack - 5);
+        }
+        return baseAttack;
+    }
 }
--- src/main/java/com/miguel/jogoheroisvsbestas/model/Orc.java
+++ src/main/java/com/miguel/jogoheroisvsbestas/model/Orc.java
@@ -4,4 +4,10 @@ public class Orc extends Beast{
     public Orc(String name, int health, int armor) {
         super(name, health, armor);
     }
+
+    @Override
+    public int calculateDefense(int attackPower, Character opponent) {
+        int modifedArmor = (int)(opponent.getArmor() *0.9);
+        return attackPower > modifedArmor ? modifedArmor : 0;
+    }
 }
